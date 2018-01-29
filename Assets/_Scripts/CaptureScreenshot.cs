f76c45cd6e858e3a5eb6920e6b9ffbc58826a229
dr-matt-smith <dr_matt_smith@me.com>
2018-01-29 08:20:53 +0000

    improved script

--- Assets/_Scripts/CaptureScreenshot.cs
+++ Assets/_Scripts/CaptureScreenshot.cs
@@ -23,7 +23,7 @@ public class CaptureScreenshot : MonoBehaviour
     public CaptureMethod captureMethod = CaptureMethod.SCREENSHOT_PNG;
 
     // Int value by which to increase the resolution of screenshots taken via CaptureScreenshot method
-    public int captureScreenshotScale = 1;  
+    public int screenshotScale = 1;  
 
     // A slider from 0 to 100 from which to set JPG quality
     [Range(0, 100)]
@@ -34,30 +34,8 @@ public class CaptureScreenshot : MonoBehaviour
     // Private Texture2D variable where to storage image capture via ReadPixels
     private Texture2D texture;
 
-    // Int variable for the screen's width
-    private int sw;
-
-    // Int variable for the screen's height
-    private int sh;
-
-    // Rect variable for the image capture area
-    private Rect sRect;
-
     // String for date and time the screenshot was taken, to be added to the image file's name
     string date;
-
-    /* ----------------------------------------
-     * At beginning of frame record the screen height, width
-     * and create a Rect object (sRect) for use by the byte-writing methods
-     */
-    void Start()
-    {
-        // record screen width / height
-        sw = Screen.width;
-        sh = Screen.height;
-        // create rect to match current screen size
-        sRect = new Rect(0,0,sw,sh);
-    }
 
     /* ----------------------------------------
      * During Update, detect if the 'P' key was pressed, 
@@ -82,8 +60,11 @@ public class CaptureScreenshot : MonoBehaviour
 
         // IF selected method is 'captureScreenshotPng'...
         if (CaptureMethod.SCREENSHOT_PNG == captureMethod){
+            // filename like: Screenshot_29-Jan-2018-07-33-26-6.png
+            string fileExtension = ".png";
+            string filename = prefix + date + fileExtension;
             // THEN use Unity's CaptureScreenshot function to capture screenshot, increased by 'captureScreenshotScale';
-            ScreenCapture.CaptureScreenshot(prefix + date + ".png", captureScreenshotScale);    
+            ScreenCapture.CaptureScreenshot(filename, screenshotScale);    
         } else {
             // ELSE, call co-routine to use one of the read-pixels methods
             StartCoroutine(ReadPixels());
@@ -102,11 +83,18 @@ public class CaptureScreenshot : MonoBehaviour
         // Wait for the end of the frame, so GUI elements are included in the screenshot
         yield return new WaitForEndOfFrame();
 
+        // screen's width & height
+        int screenWidth = Screen.width;
+        int screenHeight = Screen.height;
+
+        // Image capture area
+        Rect screenRectangle = new Rect(0, 0, screenWidth, screenHeight);
+
         // Create new Texture2D variable of the same size as the image capture area
-        texture = new Texture2D (sw,sh,TextureFormat.RGB24,false);  
+        texture = new Texture2D (screenWidth, screenHeight, TextureFormat.RGB24, false);  
 
         // Read Pixels from the capture area
-        texture.ReadPixels(sRect,0,0);
+        texture.ReadPixels(screenRectangle, 0, 0);
 
         // Apply pixels read com capture area into 'texture'
         texture.Apply();
@@ -137,11 +125,10 @@ public class CaptureScreenshot : MonoBehaviour
         // Destroy 'texture' 
         Destroy (texture);
 
-        // in form:
-        // Screenshot_29-Jan-2018-07-33-26-6.png
+        // filename like: Screenshot_29-Jan-2018-07-33-26-6.png
         string filename = prefix + date + fileExtension;
 
-        // Write bytes as file named after 'prefix", 'date' and 'format' strings
+        // Write bytes to file (in directory above "Assets")
         string path = Application.dataPath;
         path = Path.Combine(path, "..");
         path = Path.Combine(path, filename);
